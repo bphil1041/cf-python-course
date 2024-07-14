88c149efae09a0af09b446f95c811ed1d8a7378d
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-07-14 09:55:51 -0500

    Create loops.py

---
+++ Exercise_1.3/Practice_Tasks/loops.py
@@ -0,0 +1,11 @@
+# Using a for loop
+for i in range(10, 60, 10):
+    print(i)
+print("And we're done!")
+
+# Using a while loop
+i = 10
+while i <= 50:
+    print(i)
+    i += 10
+print("And we're done!")

74f0ab155203978f9a7d76480c783d6b134eddf5
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-10-07 19:34:46 -0500

    2.5

---
+++ Achievement-2/Exercise_2.2/src/books/urls.py
@@ -0,0 +1,8 @@
+# books/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Add your URL patterns here
+    path('', views.index, name='index'),
+]
--- Achievement-2/Exercise_2.2/src/books/views.py
+++ Achievement-2/Exercise_2.2/src/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# books/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome to the Books section!")
